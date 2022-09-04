2069e8d21d4d3a7045fc86e3a7b8836cef95827c
kilya <ilya.kondratyev@kupibilet.ru>
2022-09-04 15:56:16 +0300

    fix ff

--- apps/transport/toy_testing_request.rb
+++ apps/transport/toy_testing_request.rb
@@ -1,20 +1,12 @@
 module Transport
   class ToyTestingRequest
     include Import[
-              service: "context.toy_testing.service",
-              toy: "context.toy_testing.repositories.toy"
+              service: "context.toy_testing.service"
             ]
 
     def call
       puts "Hello from in_memory shop request"
-      puts "Call logic:"
-      sleep 0.5
-
       service.call
-      toy.all
-
-      sleep 0.5
-      puts "Request done"
     end
   end
 end
--- fitness_functions/main.rb
+++ fitness_functions/main.rb
@@ -2,25 +2,24 @@ require_relative  "functions/cross_context_calls_checker.rb"
 
 CONTEXT_MAP = {
   tester_account: {
-    whitelist: %w[
-      context.tester_accounting.service
-      context.tester_accounting.repositories.account
-      context.tester_accounting.repositories.toy
-    ],
-    file_paths: %w[
-      apps/transport/account_request.rb
-    ]
+    "apps/transport/account_request.rb": {
+      whitelist: %w[
+        context.tester_accounting.service
+      ]
+    }
   },
   toy_testing: {
-    whitelist: %w[
-      context.toy_testing.service
-      context.toy_testing.repositories.toy
-      context.toy_testing.repositories.account
-    ],
-    file_paths: %w[
-      apps/transport/toy_testing_request.rb
-      context/toy_testing/commands/add_toy_to_tester_account.rb
-    ]
+    "apps/transport/toy_testing_request.rb": {
+      whitelist: %w[
+        context.toy_testing.service
+      ]
+    },
+    "context/toy_testing/commands/add_toy_to_tester_account.rb": {
+      whitelist: %w[
+        context.toy_testing.repositories.toy
+        context.toy_testing.repositories.account
+      ]
+    }
   }
 }.freeze
 
@@ -29,7 +28,7 @@ CONTEXT_MAP.each do |key, context|
   puts
   puts "**** #{key} context check ****"
 
-  context[:file_paths].each do |path|
-    FitnessFunctions::Functions::CrossContextCallsChecker.new.call(path, whitelist: context[:whitelist])
+  context.each do |path, path_context|
+    FitnessFunctions::Functions::CrossContextCallsChecker.new.call(path.to_s, whitelist: path_context[:whitelist])
   end
 end
